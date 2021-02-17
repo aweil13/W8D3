ca621e8b5bc7ee8060c69fbc5d2c06c05a1fb8e1
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-02-17 15:30:48 -0500

    finished another

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -8,4 +8,28 @@ Array.prototype.uniq = function () {
     return newArray;
 }
 
+// Array.prototype.toSum = function () {
+//     let newArray = [];
+//     this.forEach(element => {
+//         if (this.includes(-element ))
+//             newArray.push([this.indexOf(element), this.indexOf(-element)])
+
+//     });
+//     return newArray.splice(0, (newArray.length) /2);
+// }
+
+Array.prototype.toSum = function () {
+    let newArray = [];
+    this.forEach(ele1 => {
+        let checked= []
+        this.forEach(ele2 => {
+            if (this.indexOf(ele2) > this.indexOf(ele1) && (ele1 + ele2 === 0) && !checked.includes(this.indexOf(ele2)))
+            newArray.push([this.indexOf(ele1), this.indexOf(ele2)]);
+            checked.push(this.indexOf(ele2));
+        });
+    });
+    return newArray;
+};
+
+
     
